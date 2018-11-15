60781b62a9830fc539cac55e63857af9c975fd66
root <root@A5-004087.localdomain>
2018-11-14 17:05:09 -0700

    created a todo list

---
+++ app.js
@@ -0,0 +1,14 @@
+var form = document.getElementById('add-item')
+var input = document.getElementById('input')
+var list = document.getElementById('list')
+
+
+function addItem(e) {
+  e.preventDefault()
+  var item = input.value
+  list.innerHTML += "<li>" + item + "</li>"
+  form.reset()
+}
+
+
+form.addEventListener('submit', addItem)
